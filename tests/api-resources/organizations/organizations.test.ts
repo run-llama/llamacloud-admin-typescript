85bcb5ad8d8e0f0ebed900c033a048ece8327618
Yong <yong@runllama.ai>
2026-08-10 17:02:36 -0700

    seal

--- tests/api-resources/organizations/organizations.test.ts
+++ tests/api-resources/organizations/organizations.test.ts
@@ -94,4 +94,28 @@ describe('resource organizations', () => {
     expect(dataAndResponse.data).toBe(response);
     expect(dataAndResponse.response).toBe(rawResponse);
   });
+
+  // Mock server tests are disabled
+  test.skip('getUsage', async () => {
+    const responsePromise = client.organizations.getUsage('182bd5e5-6e1a-4fe4-a799-aa6d9a6ab26e');
+    const rawResponse = await responsePromise.asResponse();
+    expect(rawResponse).toBeInstanceOf(Response);
+    const response = await responsePromise;
+    expect(response).not.toBeInstanceOf(Response);
+    const dataAndResponse = await responsePromise.withResponse();
+    expect(dataAndResponse.data).toBe(response);
+    expect(dataAndResponse.response).toBe(rawResponse);
+  });
+
+  // Mock server tests are disabled
+  test.skip('getUsage: request options and params are passed correctly', async () => {
+    // ensure the request options are being passed correctly by passing an invalid HTTP method in order to cause an error
+    await expect(
+      client.organizations.getUsage(
+        '182bd5e5-6e1a-4fe4-a799-aa6d9a6ab26e',
+        { get_current_invoice_total: true },
+        { path: '/_stainless_unknown_path' },
+      ),
+    ).rejects.toThrow(LlamaCloudAdmin.NotFoundError);
+  });
 });
